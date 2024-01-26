b784ae90b2b29b4e7cce994315f8ac930e2d2768
oono <tacopaz7@outlook.jp>
2024-01-26 14:13:54 +0900

    登録画面のモデルとコントローラーを追加しました

--- src/main/java/jp/KEN/yamamons/entity/Members.java
+++ src/main/java/jp/KEN/yamamons/entity/Members.java
@@ -1,5 +1,67 @@
 package jp.KEN.yamamons.entity;
 
-public class Members {
+import java.io.Serializable;
+
+public class Members implements Serializable {
+
+	private String name;
+
+	private String email;
+
+	private String phoneNumber;
+
+	private Integer credit;
+
+	private String password;
+
+	private String addres;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public Integer getCredit() {
+		return credit;
+	}
+
+	public void setCredit(Integer credit) {
+		this.credit = credit;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getAddres() {
+		return addres;
+	}
+
+	public void setAddres(String addres) {
+		this.addres = addres;
+	}
 
 }
---
+++ src/main/java/jp/KEN/yamamons/model/MembersModel.java
@@ -0,0 +1,74 @@
+package jp.KEN.yamamons.model;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.CreditCardNumber;
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import jp.KEN.yamamons.Group.Group1;
+
+public class MembersModel implements Serializable {
+
+	@NotEmpty(message="名前入力は必須です")
+	private String name;
+
+	@NotEmpty(message="emailは必須です")
+	@Email(groups=Group1.class, message="メールアドレスではありません")
+	private String email;
+
+	@NotEmpty(message="電話番号は必須です")
+	@Pattern(regexp="^[0-9]{3}-[0-9]{4}-[0-9]{4}$",message="携帯番号ではありません",groups=Group1.class)
+	private String phoneNumber;
+
+	@CreditCardNumber(message="クレジットカードではありません")
+	private Integer credit;
+
+	@NotEmpty(message="パスワードは必須です")
+	private String password;
+
+	@NotEmpty(message="住所は必須です")
+	private String addres;
+
+	public String getAddres() {
+		return addres;
+	}
+	public void setAddres(String addres) {
+		this.addres = addres;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public Integer getCredit() {
+		return credit;
+	}
+	public void setCredit(Integer credit) {
+		this.credit = credit;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+
+}
