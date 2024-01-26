b80f530246114afc839d71f30010227ebc1081e6
Yamamoto <yamamonmon1108@gmail.com>
2024-01-26 14:08:52 +0900

    daoすこし記述した

--- src/main/java/jp/KEN/yamamons/dao/LoginDao.java
+++ src/main/java/jp/KEN/yamamons/dao/LoginDao.java
@@ -1,7 +1,14 @@
 package jp.KEN.yamamons.dao;
 
+import org.springframework.stereotype.Component;
+
+import jp.KEN.yamamons.entity.Members;
+
+@Component
 public class LoginDao {
 
-
+	public int insert(Members mambers) {
+		Strint sql = "INSERT INTO members"
+	}
 
 }
