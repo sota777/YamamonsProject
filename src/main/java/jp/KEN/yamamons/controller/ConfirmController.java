1d3d39fa12e29b8dfbdb1e7333672f7d984d6960
KEN <KEN@DESKTOP-N9GORAT>
2024-01-29 14:48:20 +0900

    ConfirmController編集中

--- src/main/java/jp/KEN/yamamons/controller/ConfirmController.java
+++ src/main/java/jp/KEN/yamamons/controller/ConfirmController.java
@@ -1,5 +1,24 @@
 package jp.KEN.yamamons.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+@Controller
+@SessionAttributes({ "loginModel", "cModel" })
 public class ConfirmController {
+
+	@Autowired
+		private Dao dao;	//Item系のDaoクラス名で設定
+	
+	@RequestMapping(value = "/confirm", method = RequestMethod.GET)
+	public String toConfirm(@ModelAttribute CartModel cModel, Model model) {
+		return "rental_cart4";
+		
+	}
 
 }
